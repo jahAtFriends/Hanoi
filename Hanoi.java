bd21aae112bd2db1b6ad650a2ba08b8ab2d6b6c8
Joel Hammer <jhammer@friendsbalt.org>
2023-01-24 09:44:15 -0500

    Create Hanoi.java

---
+++ Hanoi.java
@@ -0,0 +1,22 @@
+public class Hanoi {
+  
+  /* Print out the instructions to solve the Hanoi tower of size n. */
+  public static void solveTower(int n) {
+    
+  }
+  
+  /* Recursive method to solve tower */
+  private static void solveTower(/*some params here...*/) {
+    
+  }
+  
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("Usage is java Hanoi [n] \n [n] - Number of rings");
+    }
+    
+    int n = Integer.parseInt(args[0]);
+    solveTower(n);
+  }
+  
+}
